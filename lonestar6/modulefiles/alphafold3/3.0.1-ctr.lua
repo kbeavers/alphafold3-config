e7e0c0aa97a4ee99431d08e34c9da842dd80b34e
kbeavers <kbeavers@tacc.utexas.edu>
2025-11-19 10:01:17 -0600

    Added unified memory functionality to modulefile

--- lonestar6/modulefiles/alphafold3/3.0.1-ctr.lua
+++ lonestar6/modulefiles/alphafold3/3.0.1-ctr.lua
@@ -22,17 +22,31 @@ tools it contains, please find supporting documentation at:
 
 ]]
 
-help(help_message,"\\n")
+help(help_message, "\n")
 
 whatis("Name: alphafold3")
-whatis("Version: 3.0.1")
+whatis("Version: 3.0.1-f3e86f2")
 whatis("Category: Bioinformatics")
 whatis("Keywords: Container, AlphaFold3")
 whatis("Description: AlphaFold3 run environment using TACC container image.")
 whatis("URL: https://github.com/google-deepmind/alphafold3")
 
+-- Environment vars
+setenv("AF3_HOME", "/scratch/tacc/apps/bio/alphafold3/3.0.1")
+setenv("AF3_IMAGE", "/scratch/tacc/apps/bio/alphafold3/3.0.1/image/alphafold3_3.0.1-f3e86f2.sif")
+setenv("AF3_CODE_DIR", "/scratch/tacc/apps/bio/alphafold3/3.0.1/code_TEST")
+setenv("AF3_DATABASES_DIR", "/scratch/tacc/apps/bio/alphafold3/3.0.1/data")
+
+-- Load dependencies
+always_load("tacc-apptainer")
+try_load("cuda/12.2")
+
+-- Shell function
 set_shell_function("run_alphafold3",
 "apptainer exec --nv " ..
+"  ${XLA_PYTHON_CLIENT_PREALLOCATE:+--env XLA_PYTHON_CLIENT_PREALLOCATE=$XLA_PYTHON_CLIENT_PREALLOCATE } " ..
+"  ${TF_FORCE_UNIFIED_MEMORY:+--env TF_FORCE_UNIFIED_MEMORY=$TF_FORCE_UNIFIED_MEMORY } " ..
+"  ${XLA_CLIENT_MEM_FRACTION:+--env XLA_CLIENT_MEM_FRACTION=$XLA_CLIENT_MEM_FRACTION } " ..
 "  --bind $AF3_INPUT_DIR:/root/af_input " ..
 "  --bind $AF3_OUTPUT_DIR:/root/af_output " ..
 "  --bind $AF3_MODEL_PARAMETERS_DIR:/root/models " ..
@@ -54,6 +68,9 @@ set_shell_function("run_alphafold3",
 "  ' -- \"$@\"",
 -- C-shell version
 "apptainer exec --nv " ..
+"  ${XLA_PYTHON_CLIENT_PREALLOCATE:+--env XLA_PYTHON_CLIENT_PREALLOCATE=$XLA_PYTHON_CLIENT_PREALLOCATE } " ..
+"  ${TF_FORCE_UNIFIED_MEMORY:+--env TF_FORCE_UNIFIED_MEMORY=$TF_FORCE_UNIFIED_MEMORY } " ..
+"  ${XLA_CLIENT_MEM_FRACTION:+--env XLA_CLIENT_MEM_FRACTION=$XLA_CLIENT_MEM_FRACTION } " ..
 "  --bind $AF3_INPUT_DIR:/root/af_input " ..
 "  --bind $AF3_OUTPUT_DIR:/root/af_output " ..
 "  --bind $AF3_MODEL_PARAMETERS_DIR:/root/models " ..
@@ -64,13 +81,3 @@ set_shell_function("run_alphafold3",
 "    --model_dir=/root/models " ..
 "    --db_dir=/root/public_databases " ..
 "    $*")
-
--- Environment vars
-setenv("AF3_HOME", "/scratch/tacc/apps/bio/alphafold3/3.0.1")
-setenv("AF3_IMAGE", "/scratch/tacc/apps/bio/alphafold3/3.0.1/image/alphafold_3.0.1.sif")
-setenv("AF3_CODE_DIR", "/scratch/tacc/apps/bio/alphafold3/3.0.1/code")
-setenv("AF3_DATABASES_DIR", "/scratch/tacc/apps/bio/alphafold3/3.0.1/data")
-
--- Load dependencies
-always_load("tacc-apptainer")
-try_load("cuda/12.2")
