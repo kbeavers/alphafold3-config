d0108ddf2aad8f327c4fc410c5d74719c426de62
kbeavers <kbeavers@tacc.utexas.edu>
2025-11-19 11:08:10 -0600

    Update frontera modulefile for new container image

--- frontera/modulefiles/alphafold3/3.0.1-ctr.lua
+++ frontera/modulefiles/alphafold3/3.0.1-ctr.lua
@@ -1,5 +1,5 @@
 local help_message = [[
-This is a module file for the container tacc/alphafold:3.0.1-rtx, which exposes the
+This is a module file for the container tacc/alphafold3_3.0.1-f3e86f2-rtx, which exposes the
 following program:
 
  - run_alphafold3
@@ -22,24 +22,38 @@ tools it contains, please find supporting documentation at:
 
 ]]
 
-help(help_message,"\\n")
+help(help_message, "\n")
 
 whatis("Name: alphafold3")
-whatis("Version: 3.0.1")
+whatis("Version: 3.0.1-f3e86f2-rtx")
 whatis("Category: Bioinformatics")
 whatis("Keywords: Container, AlphaFold3")
 whatis("Description: AlphaFold3 run environment using TACC container image.")
 whatis("URL: https://github.com/google-deepmind/alphafold3")
 
+-- Environment vars
+setenv("AF3_HOME", "/scratch2/projects/bio/alphafold3/3.0.1")
+setenv("AF3_IMAGE", "/scratch2/projects/bio/alphafold3/3.0.1/image/alphafold3_3.0.1-f3e86f2-rtx.sif")
+setenv("AF3_CODE_DIR", "/scratch2/projects/bio/alphafold3/3.0.1/code_TEST")
+setenv("AF3_DATABASES_DIR", "/scratch2/projects/bio/alphafold3/3.0.1/data")
+
+-- Load dependencies
+always_load("tacc-apptainer")
+try_load("cuda/12.2")
+
+-- Shell function
 set_shell_function("run_alphafold3",
 "apptainer exec --nv " ..
+"  ${XLA_PYTHON_CLIENT_PREALLOCATE:+--env XLA_PYTHON_CLIENT_PREALLOCATE=$XLA_PYTHON_CLIENT_PREALLOCATE } " ..
+"  ${TF_FORCE_UNIFIED_MEMORY:+--env TF_FORCE_UNIFIED_MEMORY=$TF_FORCE_UNIFIED_MEMORY } " ..
+"  ${XLA_CLIENT_MEM_FRACTION:+--env XLA_CLIENT_MEM_FRACTION=$XLA_CLIENT_MEM_FRACTION } " ..
+"  --env XLA_FLAGS=\"--xla_disable_hlo_passes=custom-kernel-fusion-rewriter\" " ..
 "  --bind $AF3_INPUT_DIR:/root/af_input " ..
 "  --bind $AF3_OUTPUT_DIR:/root/af_output " ..
 "  --bind $AF3_MODEL_PARAMETERS_DIR:/root/models " ..
 "  --bind $AF3_DATABASES_DIR:/root/public_databases " ..
 "  $AF3_IMAGE " ..
 "  bash -c ' " ..
-"    has_gpu=$(nvidia-smi >/dev/null 2>&1 && echo yes || echo no); " ..
 "    args=(\"$@\"); " ..
 "    for i in \"${!args[@]}\"; do " ..
 "      if [[ ${args[$i]} == --json_path=* ]]; then " ..
@@ -47,22 +61,19 @@ set_shell_function("run_alphafold3",
 "        args[$i]=--json_path=/root/af_input/$(basename \"$val\"); " ..
 "      fi; " ..
 "    done; " ..
-"    if [[ \"$has_gpu\" == \"yes\" ]]; then " ..
-"      python $AF3_CODE_DIR/run_alphafold.py " ..
-"        --output_dir=/root/af_output " ..
-"        --model_dir=/root/models " ..
-"        --db_dir=/root/public_databases " ..
-"        --flash_attention_implementation=xla " ..
-"        \"${args[@]}\"; " ..
-"    else " ..
-"      python $AF3_CODE_DIR/run_alphafold.py " ..
-"        --output_dir=/root/af_output " ..
-"        --model_dir=/root/models " ..
-"        --db_dir=/root/public_databases " ..
-"        \"${args[@]}\"; " ..
-"    fi' -- \"$@\"",
+"    python $AF3_CODE_DIR/run_alphafold.py " ..
+"      --output_dir=/root/af_output " ..
+"      --model_dir=/root/models " ..
+"      --db_dir=/root/public_databases " ..
+"      --flash_attention_implementation=xla " ..
+"      \"${args[@]}\"; " ..
+"  ' -- \"$@\"",
 -- C-shell version
 "apptainer exec --nv " ..
+"  ${XLA_PYTHON_CLIENT_PREALLOCATE:+--env XLA_PYTHON_CLIENT_PREALLOCATE=$XLA_PYTHON_CLIENT_PREALLOCATE } " ..
+"  ${TF_FORCE_UNIFIED_MEMORY:+--env TF_FORCE_UNIFIED_MEMORY=$TF_FORCE_UNIFIED_MEMORY } " ..
+"  ${XLA_CLIENT_MEM_FRACTION:+--env XLA_CLIENT_MEM_FRACTION=$XLA_CLIENT_MEM_FRACTION } " ..
+"  --env XLA_FLAGS=\"--xla_disable_hlo_passes=custom-kernel-fusion-rewriter\" " ..
 "  --bind $AF3_INPUT_DIR:/root/af_input " ..
 "  --bind $AF3_OUTPUT_DIR:/root/af_output " ..
 "  --bind $AF3_MODEL_PARAMETERS_DIR:/root/models " ..
@@ -72,15 +83,5 @@ set_shell_function("run_alphafold3",
 "    --output_dir=/root/af_output " ..
 "    --model_dir=/root/models " ..
 "    --db_dir=/root/public_databases " ..
+"    --flash_attention_implementation=xla " ..
 "    $*")
-
--- Environment vars
-setenv("AF3_HOME", "/scratch2/projects/bio/alphafold3/3.0.1")
-setenv("AF3_IMAGE", "/scratch2/projects/bio/alphafold3/3.0.1/image/alphafold3_3.0.1-rtx.sif")
-setenv("AF3_CODE_DIR", "/scratch2/projects/bio/alphafold3/3.0.1/code")
-setenv("AF3_DATABASES_DIR", "/scratch2/projects/bio/alphafold3/3.0.1/data")
-setenv("AF3_MODEL_PARAMETERS_DIR", "/scratch2/projects/bio/alphafold3/3.0.1/models")
-
--- Load dependencies
-always_load("tacc-apptainer")
-try_load("cuda/12.2")
